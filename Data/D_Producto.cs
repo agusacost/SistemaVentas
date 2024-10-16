1263bdcc35ccd5cfda8c19082a08e27606fdd8ee
agusacost <juanagustinacost@gmail.com>
2024-10-16 10:41:45 -0300

    updates

---
+++ Data/D_Producto.cs
@@ -0,0 +1,49 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class D_Producto
+    {
+        //TODO TERMINAR
+        public List<Producto> Listar()
+        {
+            List<Producto> lista = new List<Producto>();
+            using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
+            {
+                try
+                {
+                    string query = "select * from PRODUCTO";
+                    SqlCommand cmd = new SqlCommand(query, oconexion);
+                    cmd.CommandType = CommandType.Text;
+                    oconexion.Open();
+
+                    using(SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            lista.Add(new Producto()
+                            {
+                                IdProducto = Convert.ToInt32(reader["IdProducto"]),
+                                Codigo = reader["Codigo"].ToString(),
+                                Nombre = reader["Nombre"].ToString(),
+                                Descripcion = reader["Descripcion"].ToString(),
+                            });
+                        }
+                    }
+
+                }catch (Exception ex)
+                {
+                    lista = new List<Producto>();
+                }
+                return lista;
+            }
+        }
+    }
+}

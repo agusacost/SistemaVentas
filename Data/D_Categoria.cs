9bf1599cf5db08a057c6c5ec542321d9efd771ec
agusacost <juanagustinacost@gmail.com>
2024-10-17 14:36:29 -0300

    Categoria added

---
+++ Data/D_Categoria.cs
@@ -0,0 +1,49 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class D_Categoria
+    {
+        public List<Categoria> Listar()
+        {
+            List<Categoria> lista = new List<Categoria>();
+
+            using(SqlConnection oconexion = new SqlConnection(Conexion.cadena))
+            {
+                try {
+                    StringBuilder query = new StringBuilder();
+                    query.AppendLine("Select IdCategoria,Descripcion,Estado,FechaCreacion from CATEGORIA");
+
+                    SqlCommand cmd = new SqlCommand(query.ToString(), oconexion);
+                    cmd.CommandType = CommandType.Text;
+                    oconexion.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lista.Add(new Categoria()
+                            {
+                                IdCategoria = Convert.ToInt32(dr["IdCategoria"]),
+                                Descripcion = dr["Descripcion"].ToString(),
+                                Estado = Convert.ToBoolean(dr["Estado"])
+                            });
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    lista = new List<Categoria>();
+                }
+                return lista;
+            }
+        }
+    }
+}
---
+++ Negocio/N_Categoria.cs
@@ -0,0 +1,19 @@
+﻿using Data;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class N_Categoria
+    {
+        private D_Categoria objCategoria = new D_Categoria();
+        public List<Categoria> Listar()
+        {
+            return objCategoria.Listar();
+        }
+    }
+}
